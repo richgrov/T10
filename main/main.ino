c63a68889cfe44c48771ba73fe24ca58541b8510
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-12-14 00:08:52 -0700

    Use the actually correct library for motor

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,27 @@
+#include <AccelStepper.h>
 #include <Arduino.h>
-#include <Stepper.h>
 
 #define MOTOR_STEPS_PER_REV 200
 #define MICROSTEP_RESOLUTION 4
 
-Stepper stepper(MOTOR_STEPS_PER_REV, 6, 7);
+AccelStepper stepper(AccelStepper::DRIVER, 7, 6);
 
 void setup() {
    // Used to enable microstepping pin
    pinMode(2, OUTPUT);
    digitalWrite(2, HIGH);
 
-   stepper.setSpeed(300);
+   stepper.setMaxSpeed(200 * MICROSTEP_RESOLUTION);
+   stepper.setSpeed(200 * MICROSTEP_RESOLUTION);
+   stepper.setAcceleration(9999);
+   stepper.moveTo(0);
 }
 
 void loop() {
-   stepper.step(400 * MICROSTEP_RESOLUTION);
-   delay(3 * 1000);
+   if (stepper.distanceToGo() == 0) {
+      stepper.moveTo(stepper.currentPosition() + MOTOR_STEPS_PER_REV / 4 * MICROSTEP_RESOLUTION);
+      delay(3 * 1000);
+   }
+
+   stepper.run();
 }
