ebb10b90b907edde5a4157a815b61afa18153c56
Richard Grover <38992201+richgrov@users.noreply.github.com>
2024-12-21 20:50:45 -0700

    Fix memory overrun when .bss or .data are empty

--- src/main.c
+++ src/main.c
@@ -50,10 +50,12 @@ __attribute__((naked, noreturn)) void _reset(void) {
    asm volatile("  ldr r0, =_sbss\n"
                 "  ldr r1, =_ebss\n"
                 "  mov r2, #0\n"
+                "  b begin_zero_loop\n"
 
                 "zero_loop:\n"
                 "  strb r2, [r0]\n"
                 "  add r0, r0, #1\n"
+                "begin_zero_loop:\n"
                 "  cmp r0, r1\n"
                 "  blt zero_loop\n");
 
@@ -61,12 +63,14 @@ __attribute__((naked, noreturn)) void _reset(void) {
    asm volatile("  ldr r0, =_sdata\n"
                 "  ldr r1, =_sidata\n"
                 "  ldr r2, =_edata\n"
+                "  b begin_data_init_loop\n"
 
                 "data_init_loop:\n"
                 "  ldrb r3, [r1]\n"
                 "  strb r3, [r0]\n"
                 "  add r0, r0, #1\n"
                 "  add r1, r1, #1\n"
+                "begin_data_init_loop:\n"
                 "  cmp r0, r2\n"
                 "  blt data_init_loop\n");
 
