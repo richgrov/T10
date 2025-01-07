2a6c8fa9aebeeb612c22392297ff3997a0603e1e
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-01-07 14:16:46 -0700

    Abbreviate timer register fields

--- src/firmware/timer.c
+++ src/firmware/timer.c
@@ -6,22 +6,38 @@
 #include "rcc.h"
 
 typedef struct {
-   volatile uint32_t control_1;
-   volatile uint32_t control_2;
-   volatile uint32_t slave_mode_control;
-   volatile uint32_t interrupt_enable;
-   volatile uint32_t status;
-   volatile uint32_t event_generation;
-   volatile uint32_t capture_compare_mode[2];
-   volatile uint32_t capture_compare_enable;
-   volatile uint32_t counter;
-   volatile uint32_t prescaler;
-   volatile uint32_t auto_reload;
-   volatile uint32_t repitition_counter;
-   volatile uint32_t capture_compare[4];
-   volatile uint32_t break_dead_time;
-   volatile uint32_t dma_control;
-   volatile uint32_t dma_addr_full_transfer;
+   // control register 1
+   volatile uint32_t CR1;
+   // control register 2
+   volatile uint32_t CR2;
+   // control register 2
+   volatile uint32_t SMCR;
+   // slave mode control register
+   volatile uint32_t DIER;
+   // status register
+   volatile uint32_t SR;
+   // event generation register
+   volatile uint32_t EGR;
+   // capture compare mode registers
+   volatile uint32_t CCMR[2];
+   // capture compare enable register
+   volatile uint32_t CCER;
+   // counter
+   volatile uint32_t CNT;
+   // prescaler
+   volatile uint32_t PSC;
+   // auto reload register
+   volatile uint32_t ARR;
+   // repitition counter register
+   volatile uint32_t RCR;
+   // capture compare register
+   volatile uint32_t CCR[4];
+   // bread and dead time-register
+   volatile uint32_t BDTR;
+   // DMA control register
+   volatile uint32_t DCR;
+   // DMA address for full transfer
+   volatile uint32_t DMAR;
 } AdvancedControlTimer;
 
 #define TIM1 ((volatile AdvancedControlTimer *)0x40010000)
@@ -91,11 +107,10 @@ void timer_pwm_init(uint8_t timer_num, uint8_t channel) {
 
    int reg_idx = (channel - 1) / 2;
    int reg_shift = (channel - 1) % 2 * 8;
-   timer->capture_compare_mode[reg_idx] &= ~(CCM_OUT_MODE_MASK << reg_shift);
-   timer->capture_compare_mode[reg_idx] |= (CCM_OUT_PRELOAD_ENABLE | CCM_OUT_MODE_PWM_1)
-                                           << reg_shift;
-   timer->capture_compare_enable |= (1 << 4 * (channel - 1));
-   timer->break_dead_time |= BDT_MAIN_OUTPUT_ENABLE;
+   timer->CCMR[reg_idx] &= ~(CCM_OUT_MODE_MASK << reg_shift);
+   timer->CCMR[reg_idx] |= (CCM_OUT_PRELOAD_ENABLE | CCM_OUT_MODE_PWM_1) << reg_shift;
+   timer->CCER |= (1 << 4 * (channel - 1));
+   timer->BDTR |= BDT_MAIN_OUTPUT_ENABLE;
 }
 
 void timer_pwm_config(uint8_t timer_num, uint16_t prescaler, uint16_t frequency) {
@@ -110,8 +125,8 @@ void timer_pwm_config(uint8_t timer_num, uint16_t prescaler, uint16_t frequency)
       break;
    }
 
-   timer->prescaler = prescaler - 1;
-   timer->auto_reload = frequency - 1;
+   timer->PSC = prescaler - 1;
+   timer->ARR = frequency - 1;
 }
 
 void timer_pwm_duty_cycle(uint8_t timer_num, uint16_t channel, uint16_t duty_cycle) {
@@ -126,7 +141,7 @@ void timer_pwm_duty_cycle(uint8_t timer_num, uint16_t channel, uint16_t duty_cyc
       break;
    }
 
-   timer->capture_compare[channel - 1] = duty_cycle;
+   timer->CCR[channel - 1] = duty_cycle;
 }
 
 void timer_pwm_start(uint8_t timer_num) {
@@ -141,9 +156,9 @@ void timer_pwm_start(uint8_t timer_num) {
       break;
    }
 
-   timer->event_generation |= EG_UPDATE;
-   timer->control_1 |= C1_PRELOAD_AUTO_RELOAD;
-   timer->control_1 |= C1_COUNTER_ENABLE;
+   timer->EGR |= EG_UPDATE;
+   timer->CR1 |= C1_PRELOAD_AUTO_RELOAD;
+   timer->CR1 |= C1_COUNTER_ENABLE;
 }
 
 void timer_enable_update_isr(uint8_t timer_num) {
@@ -158,13 +173,13 @@ void timer_enable_update_isr(uint8_t timer_num) {
       break;
    }
 
-   timer->interrupt_enable |= 3;
-   timer->status = 0;
+   timer->DIER |= 3;
+   timer->SR = 0;
 }
 
 bool timer_is_tim1_update_now() {
-   if ((TIM1->status & 1) != 0) {
-      TIM1->status &= ~1;
+   if ((TIM1->SR & 1) != 0) {
+      TIM1->SR &= ~1;
       return true;
    }
    return false;
